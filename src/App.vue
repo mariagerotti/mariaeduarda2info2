71cb15445fcd7cbc8233ec8851be60d3d4f143d2
Felipe Rotermel <feliperotermell@gmail.com>
2022-04-18 09:07:58 -0300

    aulas vue js

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+
+export default {
+  components: { MenuLateral },
+};
+</script>
+
+<template>
+  <MenuLateral />
+  <header class="header">cabeçalho</header>
+  <main class="content">principal</main>
+  <footer class="footer">rodape</footer>
+</template>
---
+++ src/components/MenuLateral.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <aside class="sidenav">
+    <ul>
+      <li>item1</li>
+      <li>item1</li>
+      <li>item1</li>
+      <li>item1</li>
+      <li>item1</li>
+    </ul>
+  </aside>
+</template>
